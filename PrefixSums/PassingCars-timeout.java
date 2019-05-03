fe33d71c8dffd77c38d2165fbe210cf8ceb1413c
Diem To <38167774+diemtos@users.noreply.github.com>
2019-05-02 20:06:08 -0700

    Update and rename PassingCars.java to PassingCars-timeout.java

--- PrefixSums/PassingCars-timeout.java
+++ PrefixSums/PassingCars-timeout.java
@@ -2,29 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] A) {
-        
-        // Max heap
-        PriorityQueue<Integer> pQueue = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] == 0) {
-                pQueue.add(i);
-            }
-        }
-        
-        int sum = 0;
-        Iterator itr = pQueue.iterator();
-        int lastInd = A.length - 1;
-        int passCount = 0;
-        while (itr.hasNext()) {
-            sum += lastInd - (int) itr.next() - passCount;
-            passCount++;
-        }
-        
-        return sum;
-        
-        
-        /*
         Map<Integer, Integer> map = new HashMap<>();
         Iterator it;
         Map.Entry<Integer,Integer> curPair;
@@ -54,6 +31,5 @@ class Solution {
         }
         
         return sum;
-        */
     }
 }
