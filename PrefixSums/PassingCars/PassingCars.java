f8c71d2bc10647e692276b0380b64f1452822761
Diem To <38167774+diemtos@users.noreply.github.com>
2019-05-02 20:26:22 -0700

    Update PassingCars.java

--- PrefixSums/PassingCars/PassingCars.java
+++ PrefixSums/PassingCars/PassingCars.java
@@ -2,29 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] A) {
-        // Max heap
-        PriorityQueue<Integer> pQueue = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] == 0) {
-                pQueue.add(i);
-            }
+        int countOfZeros = 0, count = 0;
+
+        for (int i = 0; i < A.length; i++){
+            if (A[i] == 0) countOfZeros++;                    
+            if (A[i] == 1) count += countOfZeros;    
+            if (count > 1000000000) return -1;
         }
-        
-        int sum = 0;
-        Iterator itr = pQueue.iterator();
-        int lastInd = A.length - 1;
-        int passCount = 0;
-        while (itr.hasNext()) {
-            sum += lastInd - (int) itr.next() - passCount;
-            
-            if (sum > 1000000000) {
-                return -1;
-            }
-            
-            passCount++;
-        }
-        
-        return sum;
+        return count;
     }
 }
